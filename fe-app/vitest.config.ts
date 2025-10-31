dde2e927b7ebb5a81094c2ec145d98f9e8cba5d4
dyoun12 <gdaeyeon002@gmail.com>
2025-10-31 20:46:36 +0900

    chore:  storybook 패키지 추가

--- fe-app/vitest.config.ts
+++ fe-app/vitest.config.ts
@@ -1,22 +1,47 @@
 import { defineConfig } from "vitest/config";
 import { fileURLToPath } from "node:url";
 import { resolve } from "node:path";
+import path from 'node:path';
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import { playwright } from '@vitest/browser-playwright';
+const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const rootDir = fileURLToPath(new URL("./", import.meta.url));
-
 export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: ["./vitest.setup.tsx"],
     coverage: {
-      reporter: ["text", "html"],
+      reporter: ["text", "html"]
     },
     include: ["app/**/*.test.ts", "app/**/*.test.tsx"],
+    projects: [{
+      extends: true,
+      plugins: [
+      // The plugin will run tests for the stories defined in your Storybook config
+      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+      storybookTest({
+        configDir: path.join(dirname, '.storybook')
+      })],
+      test: {
+        name: 'storybook',
+        browser: {
+          enabled: true,
+          headless: true,
+          provider: playwright({}),
+          instances: [{
+            browser: 'chromium'
+          }]
+        },
+        setupFiles: ['.storybook/vitest.setup.ts']
+      }
+    }]
   },
   resolve: {
     alias: {
-      "@": resolve(rootDir, "."),
-    },
-  },
+      "@": resolve(rootDir, ".")
+    }
+  }
 });
